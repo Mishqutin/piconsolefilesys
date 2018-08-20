688c9a6f5bdceb71c4492f3a3fbaa5f05696aecb
Michal Stanislawski <mundzioch@wp.pl>
2018-08-20 15:06:46 +0200

    help

--- main/shell/help.py
+++ main/shell/help.py
@@ -8,6 +8,7 @@ cd <sciezka> - Przechodzi do sciezki
 cat <sciezka> - Wyswietla zawartosc pliku
 mkdir <sciezka> - Tworzy sciezke
 write <sciezka> <tekst> - Zapisuje tekst do pliku
+rm <sciezka> - Usuwa pliki i katalogi
 
 Sciezka moze byc np. taka:
 /./././home/../home/.
