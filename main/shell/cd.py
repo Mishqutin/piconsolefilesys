b2dad84725e42f8da0d811c48ab59ec025416a6c
Michal Stanislawski <mundzioch@wp.pl>
2018-08-20 14:52:46 +0200

    cd fix

--- main/shell/cd.py
+++ main/shell/cd.py
@@ -6,7 +6,7 @@ x = userPath(path)
 print(x)
 if x=="/":
     users[userdata["name"]]["dir"] = x
-elif fs.exists(x):
+elif fs.exists(x) and not fs.type(x)=="file":
     users[userdata["name"]]["dir"] = x
 else:
     c.send("No such directory!".encode())
